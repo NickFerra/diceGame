113ce109cfe59df898af0144e5a9ce089b3432bf
Nicholas Ferrari <Nick.ferrari@me.com>
2021-02-06 15:13:07 +0000

    making the dice roll add to its previous roll

--- 1player/script.js
+++ 1player/script.js
@@ -1,10 +1,11 @@
 let diceNum = document.querySelector(".player1CircleScore");
 const player1Roll = document.querySelector(".player1Roll");
-
+var randNum = 0;
 
 // rolling the dice player1
 player1Roll.addEventListener("click", () => {
 
-        let randNum = Math.floor(Math.random() * 6) +1;
+        randNum += Math.floor(Math.random() * 6) +1;
         diceNum.textContent = randNum;
+        
 })
